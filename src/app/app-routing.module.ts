76cb58f89787606c66085a98a84cea0e4b1d1968
Salih Kamaş <salihkamasy@gmail.com>
2021-03-24 23:45:52 +0300

    24.03

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 import { ProductAddComponent } from './components/product-add/product-add.component';
 import { ProductComponent } from './components/product/product.component';
+import { LoginGuard } from './guards/login.guard';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     component: ProductComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
   },
   {
     path: 'products',
@@ -20,6 +26,7 @@ const routes: Routes = [
   {
     path: 'products/add',
     component: ProductAddComponent,
+    canActivate: [LoginGuard],
   },
 ];
 
